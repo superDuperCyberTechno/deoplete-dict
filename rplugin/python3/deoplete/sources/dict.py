8ecd27db64f76d06b766fdba861cdd7c96b6bd99
superDuperCyberTechno <superDuperCyberTechno@users.noreply.github.com>
2020-05-20 11:54:30 +0200

    Update dict.py

--- rplugin/python3/deoplete/sources/dict.py
+++ rplugin/python3/deoplete/sources/dict.py
@@ -4,22 +4,22 @@ from .base import Base
 class Source(Base):
     def __init__(self, vim):
         Base.__init__(self, vim)
-
         self.rank = 1000
         self.name = 'dict'
         self.mark = '[D]'
-        self.minidict_dir = self.vim.eval('g:minidict_dir')
 
     def gather_candidates(self, context):
         candidates = []
         
-        if not self.minidict_dir:
+        if self.vim.eval('g:minidict_dir'):
+            minidict_dir = self.vim.eval('g:minidict_dir')
+        else:
             return candidates
         
-        dicts = [f for f in os.listdir(self.minidict_dir) if os.path.isfile(os.path.join(self.minidict_dir, f))]
+        dicts = [f for f in os.listdir(minidict_dir) if os.path.isfile(os.path.join(minidict_dir, f))]
         
         for dict in dicts:
-            dict = open(self.minidict_dir + '/' + dict, 'r').read().split('\n')
+            dict = open(minidict_dir + '/' + dict, 'r').read().split('\n')
             candidates = candidates + dict[0:-1]
 
         candidates = list(set(candidates))

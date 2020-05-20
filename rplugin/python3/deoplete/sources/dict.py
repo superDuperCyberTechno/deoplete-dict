0f6534482a5faf479a90b0b4aad40cb72e26d8dd
superDuperCyberTechno <superDuperCyberTechno@users.noreply.github.com>
2020-05-20 10:47:11 +0200

    Update and rename minisnip.py to dict.py

---
+++ rplugin/python3/deoplete/sources/dict.py
@@ -0,0 +1,21 @@
+import os
+from .base import Base
+
+class Source(Base):
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+
+        self.name = 'dict'
+        self.mark = '[D]'
+        self.dict_dir = self.vim.eval('g:dict_dir')
+
+    def gather_candidates(self, context):
+        dicts = [f for f in os.listdir(self.dict_dir) if os.path.isfile(os.path.join(self.dict_dir, f))]
+
+        candidates = []
+        for dict in dicts:
+            dict = open(dir+'/'+dict,'r').read().split('\n')
+            candidates = candidates + dict[0:-1]
+
+        candidates = list(set(candidates))
+        return [{'word': c} for c in candidates]
